c912ef2bdc6c3cc0612716834fdc862f2a866223
Paulo Braz <ph_braz@hotmail.com>
2024-08-05 21:02:54 +0100

    Simplified Tests

---
+++ Testing.UnitTests/MathTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using Math = UnitTesting.Fundamentals.Math;
+
+namespace UnitTestProject;
+
+[TestFixture]
+public class MathTests
+{
+    private Math _math;
+
+    [SetUp]
+    public void Setup()
+    {
+        _math = new Math();
+    }
+    
+    
+    [Test]
+    [TestCase(10, 5, 10)]
+    [TestCase(5, 10, 10)]
+    [TestCase(10, 10, 10)]
+    public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+    {
+        //Act
+        var result = _math.Max(a, b);
+        //Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+    
+    
+    [Test]
+    public void Add_WhenCalled_ReturnTheSumOfArguments()
+    {
+        //Arrange
+        const int a = 5;
+        const int b = 5;
+        
+        //Act
+        var result = _math.Add(a, b);
+        
+        //Assert
+        Assert.That(result, Is.EqualTo(10));
+    }
+}
